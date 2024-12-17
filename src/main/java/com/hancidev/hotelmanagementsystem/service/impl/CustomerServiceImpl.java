f771b0b8bd13d834762d7299a317e843adfa3e4e
Taha Hancı <taha.hanci@boun.edu.tr>
2024-12-17 22:24:33 +0300

    feat(service): add duplicate email check to createCustomer in CustomerServiceImpl

--- src/main/java/com/hancidev/hotelmanagementsystem/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/hancidev/hotelmanagementsystem/service/impl/CustomerServiceImpl.java
@@ -3,11 +3,14 @@ package com.hancidev.hotelmanagementsystem.service.impl;
 import com.hancidev.hotelmanagementsystem.dto.CustomerDto;
 import com.hancidev.hotelmanagementsystem.dto.response.CustomerResponse;
 import com.hancidev.hotelmanagementsystem.entity.Customer;
+import com.hancidev.hotelmanagementsystem.exception.CustomerAlreadyExistException;
 import com.hancidev.hotelmanagementsystem.repository.CustomerRepository;
 import com.hancidev.hotelmanagementsystem.service.CustomerService;
 import com.hancidev.hotelmanagementsystem.service.mapper.CustomerMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +21,12 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CustomerResponse createCustomer(CustomerDto customerDto) {
-        Customer customer = customerMapper.customerFromCustomerDto(customerDto);
-        customerRepository.save(customer);
+        Optional<Customer> optionalCustomer = customerRepository.findCustomerByMailAddress(customerDto.mailAddress());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistException("Customer already exists with given mail: " + customerDto.mailAddress());
+        }
+
+        Customer customer = customerRepository.save(customerMapper.customerFromCustomerDto(customerDto));
         return customerMapper.customerResponseFromCustomer(customer);
     }
 }
