8574196fb5f355ebe30365539698e589cca57409
Taha Hancı <taha.hanci@boun.edu.tr>
2024-12-20 00:53:50 +0300

    feat(controller): add endpoints for updating and finding customers in CustomerController

--- src/main/java/com/hancidev/hotelmanagementsystem/controller/CustomerController.java
+++ src/main/java/com/hancidev/hotelmanagementsystem/controller/CustomerController.java
@@ -2,13 +2,11 @@ package com.hancidev.hotelmanagementsystem.controller;
 
 import com.hancidev.hotelmanagementsystem.dto.CustomerDto;
 import com.hancidev.hotelmanagementsystem.dto.response.CustomerResponse;
+import com.hancidev.hotelmanagementsystem.dto.response.CustomerUpdateRequest;
 import com.hancidev.hotelmanagementsystem.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customers")
@@ -20,5 +18,16 @@ public class CustomerController {
     @PostMapping("/save")
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody CustomerDto customerDto) {
         return ResponseEntity.ok(customerService.createCustomer(customerDto));
+    }
+
+    @PutMapping("/update/{mailAddress}")
+    public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable String mailAddress,
+                                                           @RequestBody CustomerUpdateRequest request) {
+        return ResponseEntity.ok(customerService.updateCustomer(mailAddress, request));
+    }
+
+    @GetMapping("/find/{customerId}")
+    public ResponseEntity<CustomerResponse> findCustomer(@PathVariable String customerId) {
+        return ResponseEntity.ok(customerService.findCustomer(customerId));
     }
 }
