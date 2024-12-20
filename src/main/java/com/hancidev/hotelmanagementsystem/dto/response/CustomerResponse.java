a3eebc8c94fca4811ba298d6b19e1f9ebe8b2267
Taha Hancı <taha.hanci@boun.edu.tr>
2024-12-20 17:39:07 +0300

    feat(dto): add isActive field to CustomerResponse for representing customer status

--- src/main/java/com/hancidev/hotelmanagementsystem/dto/response/CustomerResponse.java
+++ src/main/java/com/hancidev/hotelmanagementsystem/dto/response/CustomerResponse.java
@@ -3,5 +3,6 @@ package com.hancidev.hotelmanagementsystem.dto.response;
 import lombok.Builder;
 
 @Builder
-public record CustomerResponse(String customerId, String firstName, String lastName, String mailAddress) {
+public record CustomerResponse(String customerId, String firstName, String lastName, String mailAddress,
+                               boolean isActive) {
 }
