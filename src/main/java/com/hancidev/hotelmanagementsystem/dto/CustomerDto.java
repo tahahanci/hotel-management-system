fab1246d1174fdc2dcfafc4817d9c1a0c24f3c17
Taha Hancı <taha.hanci@boun.edu.tr>
2024-12-20 17:39:47 +0300

    feat(dto): add gender field to CustomerDto for comprehensive customer data

--- src/main/java/com/hancidev/hotelmanagementsystem/dto/CustomerDto.java
+++ src/main/java/com/hancidev/hotelmanagementsystem/dto/CustomerDto.java
@@ -1,7 +1,8 @@
 package com.hancidev.hotelmanagementsystem.dto;
 
+import com.hancidev.hotelmanagementsystem.entity.enums.Gender;
 import lombok.Builder;
 
 @Builder
-public record CustomerDto(String firstName, String lastName, String mailAddress) {
+public record CustomerDto(String firstName, String lastName, String mailAddress, Gender gender) {
 }
