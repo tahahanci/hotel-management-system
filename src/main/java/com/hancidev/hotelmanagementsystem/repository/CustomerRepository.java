b9041ff6217d215c294433380c333af306c8d76a
Taha Hancı <taha.hanci@boun.edu.tr>
2024-12-20 17:38:50 +0300

    feat(repository): add queries for gender distribution and active customer retrieval

--- src/main/java/com/hancidev/hotelmanagementsystem/repository/CustomerRepository.java
+++ src/main/java/com/hancidev/hotelmanagementsystem/repository/CustomerRepository.java
@@ -1,8 +1,11 @@
 package com.hancidev.hotelmanagementsystem.repository;
 
 import com.hancidev.hotelmanagementsystem.entity.Customer;
+import com.hancidev.hotelmanagementsystem.entity.enums.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
@@ -10,4 +13,9 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findCustomerByCustomerId(String customerId);
 
     Optional<Customer> findCustomerByMailAddress(String mailAddress);
+
+    @Query("SELECT COUNT(c) FROM Customer c WHERE c.gender = :gender")
+    Long showGenderDistribution(Gender gender);
+
+    List<Customer> getCustomersByActive(boolean active);
 }
